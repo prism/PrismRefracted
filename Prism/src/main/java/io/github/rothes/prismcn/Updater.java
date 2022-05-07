409b2022f05e8d997d533b9491580378a2fdbfef
Rothes <449181985@qq.com>
2022-05-07 12:44:11 +0800

    Bump plugin version -> '3.2.0-CN-b1'

--- Prism/src/main/java/io/github/rothes/prismcn/Updater.java
+++ Prism/src/main/java/io/github/rothes/prismcn/Updater.java
@@ -19,7 +19,7 @@ import java.util.Map;
 public class Updater {
 
     private final String VERSION_CHANNCEL = "Stable";
-    private final int VERSION_NUMBER = 9;
+    private final int VERSION_NUMBER = 10;
     private final HashMap<String, Integer> msgTimesMap = new HashMap<>();
 
     public void start() {
