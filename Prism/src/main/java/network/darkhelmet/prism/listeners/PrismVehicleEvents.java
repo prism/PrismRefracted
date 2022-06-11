5141b3b152856065c0437651d705d5b71ac6b207
Rothes <449181985@qq.com>
2022-06-11 09:11:39 +0800

    Fix letter lowercase

--- Prism/src/main/java/network/darkhelmet/prism/listeners/PrismVehicleEvents.java
+++ Prism/src/main/java/network/darkhelmet/prism/listeners/PrismVehicleEvents.java
@@ -103,8 +103,9 @@ public class PrismVehicleEvents implements Listener {
             ChestBoat chestBoat = (ChestBoat) vehicle;
             for (final ItemStack item : chestBoat.getInventory().getContents()) {
                 if (item != null && item.getType() != Material.AIR) {
-                    RecordingQueue.addToQueue(ActionFactory.createItemStack("item-drop", item, item.getAmount(), -1,
-                            null, vehicle.getLocation(), chestBoat.getWoodType() + " chest boat"));
+                    RecordingQueue.addToQueue(ActionFactory.createItemStack("item-drop", item,
+                            item.getAmount(), -1, null, vehicle.getLocation(),
+                            chestBoat.getWoodType().name().toLowerCase() + " chest boat"));
                 }
             }
 
