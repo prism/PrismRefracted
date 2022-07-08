a60945719353ea1f95742cfe8cb7cccde3b7dae2
Rothes <449181985@qq.com>
2022-07-08 16:16:36 +0800

    Remove unused import

--- Prism/src/main/java/network/darkhelmet/prism/listeners/PrismEntityEvents.java
+++ Prism/src/main/java/network/darkhelmet/prism/listeners/PrismEntityEvents.java
@@ -56,7 +56,6 @@ import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.projectiles.BlockProjectileSource;
